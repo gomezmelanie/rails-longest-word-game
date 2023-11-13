2b22937f8d7fa74b155763e1c8d1ff481c9694d2
melanie gomez vidal <gomezvidal.melanie@gmail.com>
2023-11-13 21:20:35 +0100

    le mot le plus long

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  Rails.application.routes.draw do
+    get "new",           to: "games#new",      as: :new
+    post "score",        to: "games#score",    as: :score
+  end
+
 end
